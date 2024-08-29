89a8ed83ed52faa763f08a919205e01963a96455
reasonsx <kamkre01@easv365.dk>
2024-08-29 15:33:17 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -10,8 +10,8 @@ function my_custom_theme_scripts() {
         wp_enqueue_style('404-style', get_template_directory_uri() . '/css/404.css');
     } elseif (is_front_page()) {
         // Front page styles
-        wp_enqueue_style('front-page-style', get_template_directory_uri() . '/css/front-page.css');
-    } elseif (is_home() || is_post_type_archive('post')) {
+        wp_enqueue_style('index-style', get_template_directory_uri() . '/css/front-page.css');
+    } elseif (is_home()) {
         // Home page styles (and archive pages for posts)
         wp_enqueue_style('home-style', get_template_directory_uri() . '/css/home.css');
     } elseif (is_single()) {
