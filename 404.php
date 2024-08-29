ce71544306c0a00bac8eae4ac03cc1a3c7913427
reasonsx <kamkre01@easv365.dk>
2024-08-29 15:48:28 +0200

    maybe

--- 404.php
+++ 404.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+    <!-- <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>"> -->
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/404.css">
     <title>404 Error</title>
 </head>
 <body>
