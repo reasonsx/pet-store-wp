65aa443007ce80a16d50a4d60f083a5e0e83134e
reasonsx <kamkre01@easv365.dk>
2024-08-29 15:38:35 +0200

    404 update

--- 404.php
+++ 404.php
@@ -1,2 +1,9 @@
-this is 404
-test 2
+<?php get_header(); // Include the header.php file ?>
+
+<div class="error-container">
+    <h1>MIAUUUUUUUUUUUUUU</h1>
+    <img src="<?php echo get_template_directory_uri(); ?>/img/svg/error404.svg" alt="">
+    <a href="<?php echo home_url(); ?>" class="btn">Go back</a>
+</div>
+
+<?php get_footer(); // Include the footer.php file ?>
