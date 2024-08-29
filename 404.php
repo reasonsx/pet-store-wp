a9cdb72dafec80ca4888f38ef81d952024253d17
reasonsx <kamkre01@easv365.dk>
2024-08-29 15:39:26 +0200

    404 up 2

--- 404.php
+++ 404.php
@@ -1,4 +1,3 @@
-<?php get_header(); // Include the header.php file ?>
 
 <div class="error-container">
     <h1>MIAUUUUUUUUUUUUUU</h1>
@@ -6,4 +5,3 @@
     <a href="<?php echo home_url(); ?>" class="btn">Go back</a>
 </div>
 
-<?php get_footer(); // Include the footer.php file ?>
