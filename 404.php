c6e8a3a53c9e4d896f0697fcb0e863b4fc701b6b
reasonsx <kamkre01@easv365.dk>
2024-08-29 15:41:13 +0200

    d

--- 404.php
+++ 404.php
@@ -1,7 +1,7 @@
-
+<?php
 <div class="error-container">
     <h1>MIAUUUUUUUUUUUUUU</h1>
     <img src="<?php echo get_template_directory_uri(); ?>/img/svg/error404.svg" alt="">
     <a href="<?php echo home_url(); ?>" class="btn">Go back</a>
 </div>
-
+?>
