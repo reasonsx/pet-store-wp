c77b4a7df806f0ee20c8b868c2be88d11a95594f
reasonsx <kamkre01@easv365.dk>
2024-09-02 13:36:53 +0200

    check

--- single.php
+++ single.php
@@ -1,36 +1,48 @@
 <?php get_header(); // Include the header ?>
 <div class="blog-post">
-<div class="blog-post-start">
-    <h1 class="blog-post__title"><?php the_title(); ?></h1>
-    <div class="categories">
-        <?php
-        $categories = get_the_category();
-        if ($categories) {
-            foreach ($categories as $category) {
-                echo '<h2 class="category">' . esc_html($category->name) . '</h2>';
+    <div class="blog-post-start">
+        <h1 class="blog-post__title"><?php the_title(); ?></h1>
+        <div class="categories">
+            <?php
+            $categories = get_the_category();
+            if ($categories) {
+                foreach ($categories as $category) {
+                    echo '<h2 class="category">' . esc_html($category->name) . '</h2>';
+                }
             }
-        }
-        ?>
+            ?>
+        </div>
+
+        <p class="blog-post__info">
+            <a href="#"><?php echo esc_html(get_the_date()); ?></a> by 
+            <?php 
+            $author_id = get_the_author_meta('ID'); 
+            $author_url = get_author_posts_url($author_id); 
+            $author_name = get_the_author();
+            
+            // Debugging output
+            if ($author_id && $author_name) {
+                echo '<a href="' . esc_url($author_url) . '">' . esc_html($author_name) . '</a>';
+            } else {
+                echo '<span>Author information not available</span>';
+            }
+            ?>
+        </p>
+
+        <div class="hashtag">
+            <?php
+            $tags = get_the_tags();
+            if ($tags) {
+                foreach ($tags as $tag) {
+                    echo '<h6>' . esc_html($tag->name) . '</h6>';
+                }
+            }
+            ?>
+        </div>
     </div>
 
-    <p class="blog-post__info">
-        <a href="#"><?php echo get_the_date(); ?></a> by <a
-            href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author(); ?></a>
-    </p>
-    <div class="hashtag">
-        <?php
-        $tags = get_the_tags();
-        if ($tags) {
-            foreach ($tags as $tag) {
-                echo '<h6>' . esc_html($tag->name) . '</h6>';
-            }
-        }
-        ?>
-    </div>
+    <p class="text-introduction"><?php the_content(); ?></p>
 </div>
-<p class="text-introduction"><?php the_content(); ?></p>
-</div>
-
 
 <div class="comments">
     <?php if (comments_open() || get_comments_number()): ?>
