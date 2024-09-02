8f53dcd0436ffa5cf73df0b8a59e811442c5d2ac
reasonsx <kamkre01@easv365.dk>
2024-09-02 13:15:48 +0200

    Update single.php

--- single.php
+++ single.php
@@ -26,7 +26,16 @@
         
     <div class="blog-post-start">
         <h1 class="blog-post__title"><?php the_title(); ?></h1>
-        <h2 class="blog-post__category"><?php the_category(); ?></h2>
+        <div class="categories">
+        <?php
+        $categories = get_the_category();
+        if ($categories) {
+            foreach ($categories as $category) {
+                echo '<h2 class="category">' . esc_html($category->name) . '</h2>';
+            }
+        }
+        ?>
+    </div>
         <p class="blog-post__info">
             <a href="#"><?php echo get_the_date(); ?></a> by <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author(); ?></a>
         </p>
