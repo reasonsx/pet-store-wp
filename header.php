f5ce233d7f23ed09436b6175d2524c5f5bb9cbed
reasonsx <kamkre01@easv365.dk>
2024-09-04 08:55:06 +0200

    Update header.php

--- header.php
+++ header.php
@@ -13,6 +13,7 @@
             <li><a href="<?php echo home_url('/'); ?>">Home</a></li>
             <li><a href="">Store</a></li>
             <li><a href="<?php echo home_url('/blog/'); ?>">Pet blog</a></li>
+            <?php pll_the_languages(); ?>
         </ul>
         <div class="cart">
 
