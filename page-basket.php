162193c33bfa5461daea0d4eddc55bb4659bf5db
SirbuMadalina <madsir01@easv365.dk>
2024-10-11 20:15:22 +0200

    basket page

--- page-basket.php
+++ page-basket.php
@@ -1,3 +1,41 @@
 <?php get_header(); ?>
     <?php echo do_shortcode('[woocommerce_cart]'); ?>
 <?php get_footer(); ?>
+
+
+<div class="basket-container">
+    <h1 class="basket-title">Your Basket</h1>
+    
+    <div class="basket-items">
+        <!-- Basket items will be displayed here -->
+        <?php
+        // Query for basket items and display them
+        if( have_posts() ) {
+            while( have_posts() ) {
+                the_post();
+                ?>
+                <div class="basket-item">
+                    <h2><?php the_title(); ?></h2>
+                    <div class="item-description">
+                        <?php the_content(); ?>
+                    </div>
+                    <div class="item-price">
+                        Price: $<?php echo get_post_meta(get_the_ID(), 'price', true); ?>
+                    </div>
+                </div>
+                <?php
+            }
+        } else {
+            echo '<p>No items in the basket.</p>';
+        }
+        ?>
+    </div>
+    
+    <div class="basket-footer">
+        <a href="#" class="checkout-button">Proceed to Checkout</a>
+    </div>
+</div>
+
+<?php
+get_footer();
+?>
