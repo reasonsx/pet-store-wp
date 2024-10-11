018824d4f1e785f1ea6505392cdb408ab0c9ad7e
SirbuMadalina <madsir01@easv365.dk>
2024-10-11 20:40:51 +0200

    removed code

--- page-basket.php
+++ page-basket.php
@@ -1,33 +1,5 @@
 <?php get_header(); ?>
-    <?php echo do_shortcode('[woocommerce_cart]'); ?>
-
-
-<div class="basket-container">
-    <div class="basket-items">
-        <!-- Basket items will be displayed here -->
-        <?php
-        // Query for basket items and display them
-        if( have_posts() ) {
-            while( have_posts() ) {
-                the_post();
-                ?>
-                <div class="basket-item">
-                    <h2><?php the_title(); ?></h2>
-                    <div class="item-description">
-                        <?php the_content(); ?>
-                    </div>
-                    
-                </div>
-                <?php
-            }
-        } else {
-            echo '<p>No items in the basket.</p>';
-        }
-        ?>
+    <div class="basket-container">
+        <?php echo do_shortcode('[woocommerce_cart]'); ?>
     </div>
-
-</div>
-
-<?php
-get_footer();
-?>
+<?php get_footer(); ?>
