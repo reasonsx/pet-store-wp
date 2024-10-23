ecc690c8d4dfb1359dec7f9c4114b1dccca9975e
reasonsx <kamkre01@easv365.dk>
2024-10-23 10:38:18 +0200

    Update header.php

--- header.php
+++ header.php
@@ -18,70 +18,66 @@
             <li><a href="<?php echo home_url('/'); ?>">Home</a></li>
             <li><a href="#">Store</a></li>
             <li><a href="<?php echo home_url('/blog'); ?>">Pet blog</a></li>
-
-            <!-- Language Switcher with Inline SVG Flags -->
-            <li>
-                <div class="language-switcher">
-                    <?php
-                    $languages = pll_the_languages(array('raw' => 1)); // Get the available languages in array format
-                    
-                    foreach ($languages as $language) :
-                        if ($language['slug'] == 'en') {
-                            // UK flag SVG for English
-                            echo '<a href="' . esc_url($language['url']) . '">
-                                    <svg width="24" height="16" viewBox="0 0 1200 600" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <g clip-path="url(#clip0_20_32)">
-                                            <mask id="mask0_20_32" style="mask-type:luminance" maskUnits="userSpaceOnUse" x="0" y="0" width="1200" height="600">
-                                                <path d="M0 0V600H1200V0H0Z" fill="white"/>
-                                            </mask>
-                                            <g mask="url(#mask0_20_32)">
-                                                <path d="M0 0V600H1200V0H0Z" fill="#012169"/>
-                                                <path d="M0 0L1200 600ZM1200 0L0 600Z" fill="black"/>
-                                                <path d="M0 0L1200 600M1200 0L0 600" stroke="white" stroke-width="120"/>
-                                                <mask id="mask1_20_32" style="mask-type:luminance" maskUnits="userSpaceOnUse" x="0" y="0" width="1200" height="600">
-                                                    <path d="M600 300H1200V600L600 300ZM600 300V600H0L600 300ZM600 300H0V0L600 300ZM600 300V0H1200L600 300Z" fill="white"/>
-                                                </mask>
-                                                <g mask="url(#mask1_20_32)">
-                                                    <path d="M0 0L1200 600ZM1200 0L0 600Z" fill="black"/>
-                                                    <path d="M0 0L1200 600M1200 0L0 600" stroke="#C8102E" stroke-width="80"/>
-                                                </g>
-                                                <path d="M600 0V600ZM0 300H1200Z" fill="black"/>
-                                                <path d="M600 0V600M0 300H1200" stroke="white" stroke-width="200"/>
-                                                <path d="M600 0V600ZM0 300H1200Z" fill="black"/>
-                                                <path d="M600 0V600M0 300H1200" stroke="#C8102E" stroke-width="120"/>
-                                            </g>
-                                        </g>
-                                        <defs>
-                                            <clipPath id="clip0_20_32">
-                                                <rect width="1200" height="600" fill="white"/>
-                                            </clipPath>
-                                        </defs>
-                                    </svg>
-                                  </a>';
-                        } elseif ($language['slug'] == 'da') {
-                            // Danish flag SVG for Danish
-                            echo '<a href="' . esc_url($language['url']) . '">
-                                    <svg width="24" height="16" viewBox="0 0 37 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <g clip-path="url(#clip0_20_29)">
-                                            <path d="M0 0H37V28H0V0Z" fill="#C8102E"/>
-                                            <path d="M0 14H37ZM14 0V28Z" fill="black"/>
-                                            <path d="M0 14H37M14 0V28" stroke="white" stroke-width="4"/>
-                                        </g>
-                                        <defs>
-                                            <clipPath id="clip0_20_29">
-                                                <rect width="37" height="28" fill="white"/>
-                                            </clipPath>
-                                        </defs>
-                                    </svg>
-                                  </a>';
-                        }
-                    endforeach;
-                    ?>
-                </div>
-            </li>
         </ul>
 
-        <div class="cart"></div>
+        <!-- Language Switcher with Inline SVG Flags -->
+        <div class="language-switcher">
+            <?php
+            $languages = pll_the_languages(array('raw' => 1)); // Get the available languages in array format
+            
+            foreach ($languages as $language) :
+                if ($language['slug'] == 'en') {
+                    // UK flag SVG for English
+                    echo '<a href="' . esc_url($language['url']) . '">
+                            <svg width="24" height="16" viewBox="0 0 1200 600" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g clip-path="url(#clip0_20_32)">
+                                    <mask id="mask0_20_32" style="mask-type:luminance" maskUnits="userSpaceOnUse" x="0" y="0" width="1200" height="600">
+                                        <path d="M0 0V600H1200V0H0Z" fill="white"/>
+                                    </mask>
+                                    <g mask="url(#mask0_20_32)">
+                                        <path d="M0 0V600H1200V0H0Z" fill="#012169"/>
+                                        <path d="M0 0L1200 600ZM1200 0L0 600Z" fill="black"/>
+                                        <path d="M0 0L1200 600M1200 0L0 600" stroke="white" stroke-width="120"/>
+                                        <mask id="mask1_20_32" style="mask-type:luminance" maskUnits="userSpaceOnUse" x="0" y="0" width="1200" height="600">
+                                            <path d="M600 300H1200V600L600 300ZM600 300V600H0L600 300ZM600 300H0V0L600 300ZM600 300V0H1200L600 300Z" fill="white"/>
+                                        </mask>
+                                        <g mask="url(#mask1_20_32)">
+                                            <path d="M0 0L1200 600ZM1200 0L0 600Z" fill="black"/>
+                                            <path d="M0 0L1200 600M1200 0L0 600" stroke="#C8102E" stroke-width="80"/>
+                                        </g>
+                                        <path d="M600 0V600ZM0 300H1200Z" fill="black"/>
+                                        <path d="M600 0V600M0 300H1200" stroke="white" stroke-width="200"/>
+                                        <path d="M600 0V600ZM0 300H1200Z" fill="black"/>
+                                        <path d="M600 0V600M0 300H1200" stroke="#C8102E" stroke-width="120"/>
+                                    </g>
+                                </g>
+                                <defs>
+                                    <clipPath id="clip0_20_32">
+                                        <rect width="1200" height="600" fill="white"/>
+                                    </clipPath>
+                                </defs>
+                            </svg>
+                          </a>';
+                } elseif ($language['slug'] == 'da') {
+                    // Danish flag SVG for Danish
+                    echo '<a href="' . esc_url($language['url']) . '">
+                            <svg width="24" height="16" viewBox="0 0 37 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g clip-path="url(#clip0_20_29)">
+                                    <path d="M0 0H37V28H0V0Z" fill="#C8102E"/>
+                                    <path d="M0 14H37ZM14 0V28Z" fill="black"/>
+                                    <path d="M0 14H37M14 0V28" stroke="white" stroke-width="4"/>
+                                </g>
+                                <defs>
+                                    <clipPath id="clip0_20_29">
+                                        <rect width="37" height="28" fill="white"/>
+                                    </clipPath>
+                                </defs>
+                            </svg>
+                          </a>';
+                }
+            endforeach;
+            ?>
+        </div>
     </nav>
 
     <style>
@@ -129,20 +125,15 @@
             color: #f0a500;
         }
 
-        nav .cart {
-            position: relative;
-        }
-
-        nav .cart::before {
-            content: "🛒";
-            font-size: 24px;
-            color: #fff;
-        }
-
         /* Language Switcher Styling */
+        .language-switcher {
+            display: flex;
+            gap: 15px;
+        }
+
         .language-switcher svg {
             width: 24px;
-            height: 16px; /* Same height for both flags */
+            height: 16px;
             cursor: pointer;
         }
 
@@ -165,6 +156,10 @@
 
             nav ul li a {
                 font-size: 20px;
+            }
+
+            .language-switcher {
+                margin-top: 10px;
             }
         }
     </style>
